e870d750b435856917170b6e8635bbdcf6e8a7a7
Bryan Wandrych <bdwandry@mtu.edu>
2021-06-15 14:13:23 -0400

    Added Base Logic - Currently Working

--- src/MultiThreadedBigIntegerBasedPrimeNumberGenerator.java
+++ src/MultiThreadedBigIntegerBasedPrimeNumberGenerator.java
@@ -1,6 +1,129 @@
-public class MultiThreadedBigIntegerBasedPrimeNumberGenerator {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.math.BigInteger;
 
-    public static void main(String[] args) {
-        
+class MultithreadCalculate extends Thread {
+    BigInteger PrimeNumCalculate = new BigInteger("-1");
+    BigInteger indexNum = new BigInteger("-1");
+
+    public BigInteger getPrimeNumCalculate() {
+        return PrimeNumCalculate;
+    }
+
+    public void setPrimeNumCalculate(BigInteger primeNumCalculate) {
+        PrimeNumCalculate = primeNumCalculate;
+    }
+
+    public BigInteger getIndexNum() {
+        return indexNum;
+    }
+
+    public void setIndexNum(BigInteger indexNum) {
+        this.indexNum = indexNum;
+    }
+
+    public void run() {
+        try {
+            boolean isPrime = true;
+            BigInteger staticNumChecker = new BigInteger("2");
+            int compare = -1;
+            while (compare != 0 ) {
+                compare = staticNumChecker.compareTo(getPrimeNumCalculate());
+
+                if (compare < 0) {
+                    BigInteger modResult = getPrimeNumCalculate().mod(staticNumChecker);
+                    if (modResult.equals(MultiThreadedBigIntegerBasedPrimeNumberGenerator.mod0)) {
+                        isPrime = false;
+                        MultiThreadedBigIntegerBasedPrimeNumberGenerator.primeArray[0][getIndexNum().intValue()] = getPrimeNumCalculate();
+                        MultiThreadedBigIntegerBasedPrimeNumberGenerator.primeArray[1][getIndexNum().intValue()] = BigInteger.valueOf(0);
+                        break;
+                    }
+                }
+
+                staticNumChecker = staticNumChecker.add(BigInteger.valueOf(1));
+            }
+
+            if (isPrime) {
+                MultiThreadedBigIntegerBasedPrimeNumberGenerator.primeArray[0][getIndexNum().intValue()] = getPrimeNumCalculate();
+                MultiThreadedBigIntegerBasedPrimeNumberGenerator.primeArray[1][getIndexNum().intValue()] = BigInteger.valueOf(1);
+            }
+
+            System.out.println("Thread " + Thread.currentThread().getId() + "; Index: " + getIndexNum() + "; Number: " + getPrimeNumCalculate() + "; isPrime: " + isPrime);
+        }
+        catch (Exception e) {
+            System.out.println("Exception is caught");
+        }
+    }
+}
+
+public class MultiThreadedBigIntegerBasedPrimeNumberGenerator {
+    static public BigInteger primeBase = new BigInteger("1");
+    static public BigInteger [][] primeArray;
+    static public BigInteger totalPrimeCounter = new BigInteger("0");
+    static public BigInteger mod2 = new BigInteger("2");
+    static public BigInteger mod0 = new BigInteger("0");
+    private static File file = null;
+    private static PrintWriter out = null;
+    private static int cores;
+
+    private static void fillArray() {
+        for (int i = 0; i < cores; i++) {
+            primeArray[0][i] = BigInteger.valueOf(-1);
+        }
+
+        for (int i = 0; i < cores; i++) {
+            primeArray[1][i] = BigInteger.valueOf(-1);
+        }
+    }
+
+    private static void CalculatePrimeNumberInBatches() {
+        while (true) {
+            primeArray = new BigInteger[2][cores];
+            fillArray();
+            for (int i = 0; i < cores; i++) {
+                MultithreadCalculate multithreadCalculate = new MultithreadCalculate();
+                primeBase = primeBase.add(mod2);
+                multithreadCalculate.setPrimeNumCalculate(primeBase);
+                multithreadCalculate.setIndexNum(BigInteger.valueOf(i));
+                multithreadCalculate.start();
+            }
+
+            while (true) {
+                boolean flag = false;
+                for (int i = 0; i < cores; i++) {
+                    if ((primeArray[1][i].equals(mod0) || primeArray[1][i].equals(BigInteger.valueOf(1)))) {
+                        flag = true;
+                    } else {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) {
+                    break;
+                }
+            }
+
+            PrintPrimes();
+        }
+    }
+
+    private static void PrintPrimes() {
+        for (int i = 0; i < cores; i++) {
+            if (primeArray[1][i].equals(BigInteger.valueOf(1))) {
+                totalPrimeCounter = totalPrimeCounter.add(BigInteger.valueOf(1));
+                out.println(totalPrimeCounter + ". Prime Number: " + primeArray[0][i]);
+            }
+        }
+
+        out.flush();
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        file = new File(System.getProperty("user.home") + "/Desktop" + "/PrimeNumber.txt");
+        out = new PrintWriter(file);
+        cores = Runtime.getRuntime().availableProcessors();
+
+        CalculatePrimeNumberInBatches();
     }
 }
